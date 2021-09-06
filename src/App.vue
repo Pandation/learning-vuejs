db4dfde922051d1acd4d716745689775e042fe8b
Pandation <f.baumes@laposte.net>
2021-09-07 01:22:15 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>CECI EST MON HEADER</h1>
+    </header>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footer><h1>CECI EST MON FOOTER</h1></footer>
   </div>
 </template>
 
