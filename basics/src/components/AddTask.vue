18aeca33c5fb1bbf6c8dfd64ec3fe26211388546
Pandation <f.baumes@laposte.net>
2021-09-13 13:01:46 +0200

    add styles to navigation

--- basics/src/components/AddTask.vue
+++ basics/src/components/AddTask.vue
@@ -24,34 +24,34 @@
 
 <script>
 export default {
-  name: 'AddTask',
+  name: "AddTask",
   data() {
     return {
-      text: '',
-      day: '',
+      text: "",
+      day: "",
       reminder: false,
-    }
+    };
   },
   methods: {
     onSubmit(e) {
-      e.preventDefault()
+      e.preventDefault();
       if (!this.text) {
-        alert('Please add a task')
-        return
+        alert("Please add a task");
+        return;
       }
       const newTask = {
         // id: Math.floor(Math.random() * 100000),
         text: this.text,
         day: this.day,
         reminder: this.reminder,
-      }
-      this.$emit('add-task', newTask)
-      this.text = ''
-      this.day = ''
-      this.reminder = false
+      };
+      this.$emit("add-task", newTask);
+      this.text = "";
+      this.day = "";
+      this.reminder = false;
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -65,9 +65,10 @@ export default {
   display: block;
 }
 .form-control input {
+  box-sizing: border-box;
   width: 100%;
   height: 40px;
-  margin: 5px;
+  margin-top: 2px;
   padding: 3px 7px;
   font-size: 17px;
 }
--- basics/src/components/Header.vue
+++ basics/src/components/Header.vue
@@ -7,10 +7,23 @@
 
 <script>
 export default {
-
+  name: "Header"
 }
 </script>
 
-<style>
+<style scoped>
+  nav {
+    display: flex;
+    width: 300px;
+    justify-content: space-evenly;
+  }
+  a {
+      text-decoration: none;
+      font-weight: bold;
+      font-size: 1.8em;
+      margin: 20px;
+      color: rgb(31, 156, 104);
+      text-shadow: 0 -1px rgb(19, 41, 78);
+    }
 
 </style>
